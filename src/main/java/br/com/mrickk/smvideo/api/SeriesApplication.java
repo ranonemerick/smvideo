bad016f0576644ea54b2e08ace69bbff82bcd0fc
Ranon Emerick <ranonemerick@gmail.com>
2024-02-29 12:09:28 -0300

    Criacao da classe Principal

--- src/main/java/br/com/mrickk/smvideo/api/SeriesApplication.java
+++ src/main/java/br/com/mrickk/smvideo/api/SeriesApplication.java
@@ -1,16 +1,9 @@
 package br.com.mrickk.smvideo.api;
 
-import br.com.mrickk.smvideo.api.model.DadosEpisodio;
-import br.com.mrickk.smvideo.api.model.DadosSerie;
-import br.com.mrickk.smvideo.api.model.DadosTemporada;
-import br.com.mrickk.smvideo.api.service.ConsumoApi;
-import br.com.mrickk.smvideo.api.service.ConverteDados;
+import br.com.mrickk.smvideo.api.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class SeriesApplication implements CommandLineRunner {
@@ -21,28 +14,7 @@ public class SeriesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&apikey=e703c90b");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&season=1&episode=2&apikey=e703c90b");
-
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for(int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&season=" + i
-					+"&apikey=e703c90b");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ src/main/java/br/com/mrickk/smvideo/api/principal/Principal.java
@@ -0,0 +1,36 @@
+package br.com.mrickk.smvideo.api.principal;
+
+import br.com.mrickk.smvideo.api.model.DadosSerie;
+import br.com.mrickk.smvideo.api.model.DadosTemporada;
+import br.com.mrickk.smvideo.api.service.ConsumoApi;
+import br.com.mrickk.smvideo.api.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner sc = new Scanner(System.in);
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private final String ENDERECO = "http://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=e703c90b";
+    private ConverteDados conversor = new ConverteDados();
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série: ");
+        var nomeSerie = sc.nextLine();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ","+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ","+")
+                    + "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+    }
+}
