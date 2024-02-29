d740d91dc2ae9bbf73c46ec5898dfedea90668ca
Ranon Emerick <ranonemerick@gmail.com>
2024-02-29 11:38:16 -0300

    Criacao DTO DadosTemporada + metodo para obter dados

--- src/main/java/br/com/mrickk/smvideo/api/SeriesApplication.java
+++ src/main/java/br/com/mrickk/smvideo/api/SeriesApplication.java
@@ -2,11 +2,15 @@ package br.com.mrickk.smvideo.api;
 
 import br.com.mrickk.smvideo.api.model.DadosEpisodio;
 import br.com.mrickk.smvideo.api.model.DadosSerie;
+import br.com.mrickk.smvideo.api.model.DadosTemporada;
 import br.com.mrickk.smvideo.api.service.ConsumoApi;
 import br.com.mrickk.smvideo.api.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SeriesApplication implements CommandLineRunner {
@@ -29,5 +33,16 @@ public class SeriesApplication implements CommandLineRunner {
 
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados("http://www.omdbapi.com/?t=Breaking+Bad&season=" + i
+					+"&apikey=e703c90b");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
 	}
 }
