a42568136b797ca1c13a86693948bab8643dcae1
Ranon Emerick <ranonemerick@gmail.com>
2024-02-29 10:31:19 -0300

    Consumo Api OMDb

---
+++ src/main/java/br/com/mrickk/smvideo/api/SeriesApplication.java
@@ -0,0 +1,20 @@
+package br.com.mrickk.smvideo.api;
+
+import br.com.mrickk.smvideo.api.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=e703c90b");
+	}
+}
---
+++ src/test/java/br/com/mrickk/series/series/SeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrickk.series.series;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
