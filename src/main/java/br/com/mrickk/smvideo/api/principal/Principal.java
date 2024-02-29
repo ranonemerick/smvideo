c64a18bd04d0d02c2f76a06cac3fcc99c93a5733
Ranon Emerick <ranonemerick@gmail.com>
2024-02-29 16:05:53 -0300

    Metodo para listar episodios por temporada

--- src/main/java/br/com/mrickk/smvideo/api/principal/Principal.java
+++ src/main/java/br/com/mrickk/smvideo/api/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.mrickk.smvideo.api.principal;
 
+import br.com.mrickk.smvideo.api.model.DadosEpisodio;
 import br.com.mrickk.smvideo.api.model.DadosSerie;
 import br.com.mrickk.smvideo.api.model.DadosTemporada;
 import br.com.mrickk.smvideo.api.service.ConsumoApi;
@@ -16,21 +17,32 @@ public class Principal {
     private final String ENDERECO = "http://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=e703c90b";
     private ConverteDados conversor = new ConverteDados();
+
     public void exibeMenu() {
         System.out.println("Digite o nome da série: ");
+
         var nomeSerie = sc.nextLine();
         var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ","+") + API_KEY);
+
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for(int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ","+")
+        for(int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ","+")
                     + "&season=" + i + API_KEY);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+        for(int i = 0; i < dados.totalTemporadas(); i++) {
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++ ) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
+
     }
 }
