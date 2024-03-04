103e80b760273203082dc64faf782f2dff27bd3c
Ranon Emerick <ranonemerick@gmail.com>
2024-03-04 10:32:06 -0300

    Ajuste DTO + criacao classe Serie + criacao ENUM para genero

---
+++ src/main/java/br/com/mrickk/smvideo/api/model/Categoria.java
@@ -0,0 +1,9 @@
+package br.com.mrickk.smvideo.api.model;
+
+public enum Categoria {
+    ACAO,
+    ROMANCE,
+    COMEDIA,
+    DRAMA,
+    CRIME;
+}
---
+++ src/main/java/br/com/mrickk/smvideo/api/model/Serie.java
@@ -0,0 +1,15 @@
+package br.com.mrickk.smvideo.api.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Serie {
+
+    private String titulo;
+    private Integer totalTemporadas;
+    private Double avaliacao;
+    private Categoria genero;
+    private String atores;
+    private String poster;
+    private String sinopse;
+    
+}
