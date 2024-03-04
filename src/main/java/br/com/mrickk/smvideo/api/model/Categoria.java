2c5efae29ee7aa9d9625f488c80a9d9bd2b6df39
Ranon Emerick <ranonemerick@gmail.com>
2024-03-04 10:45:04 -0300

    Ajuste classe Serie + metodo na classe ENUM Categoria

--- src/main/java/br/com/mrickk/smvideo/api/model/Categoria.java
+++ src/main/java/br/com/mrickk/smvideo/api/model/Categoria.java
@@ -1,9 +1,25 @@
 package br.com.mrickk.smvideo.api.model;
 
 public enum Categoria {
-    ACAO,
-    ROMANCE,
-    COMEDIA,
-    DRAMA,
-    CRIME;
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
 }
--- src/main/java/br/com/mrickk/smvideo/api/model/Serie.java
+++ src/main/java/br/com/mrickk/smvideo/api/model/Serie.java
@@ -1,7 +1,15 @@
 package br.com.mrickk.smvideo.api.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.*;
 
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Serie {
 
     private String titulo;
@@ -11,5 +19,16 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
+
+
+    public Serie(DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
     
 }
