1f879b100c793585c2d2ab5f8cf15ff73522d53b
Ranon Emerick <ranonemerick@gmail.com>
2024-03-04 12:00:30 -0300

    Ajuste de classe para adequar à JPA

--- src/main/java/br/com/mrickk/smvideo/api/model/Serie.java
+++ src/main/java/br/com/mrickk/smvideo/api/model/Serie.java
@@ -2,11 +2,14 @@ package br.com.mrickk.smvideo.api.model;
 
 import br.com.mrickk.smvideo.api.service.ConsultaChatGPT;
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Optional;
 import java.util.OptionalDouble;
 
+@Entity(name = "Serie")
+@Table(name = "series")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,10 +17,17 @@ import java.util.OptionalDouble;
 @ToString
 public class Serie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
+
     private String atores;
     private String poster;
     private String sinopse;
